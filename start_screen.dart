5d5c1296bbbd07ef84e06f76b3a551aff6b783e8
Ankitha C <143744388+AnkithaC46@users.noreply.github.com>
2024-05-05 19:19:45 +0530

    Add files via upload

---
+++ start_screen.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class StartScreen extends StatelessWidget {
+  const StartScreen(this.startQuiz, {super.key});
+
+  final void Function() startQuiz;
+
+  @override
+  Widget build(context) {
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Opacity(
+            opacity: 0.6,
+            child: Image.asset(
+              'assets/images/logo.jpg',
+              width: 300,
+            ),
+          ),
+          const SizedBox(
+            height: 80,
+          ),
+          Text(
+            'Learn Futter the fun way!',
+            style: GoogleFonts.lato(
+              color: Colors.white,
+              fontSize: 24,
+            ),
+          ),
+          const SizedBox(
+            height: 30,
+          ),
+          OutlinedButton.icon(
+            onPressed: startQuiz,
+            style: OutlinedButton.styleFrom(
+              foregroundColor: Colors.white,
+            ),
+            icon: const Icon(Icons.arrow_right_alt),
+            label: const Text('Start Quiz'),
+          ),
+        ],
+      ),
+    );
+  }
+}
